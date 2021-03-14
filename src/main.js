4d2550e45dbd2585d036ee2d4c984154e0b6ac5f
Dmitriy Sidorov <dmitrii.sidorov@accenture.com>
2021-03-14 16:19:10 +0300

    connected firebase

--- src/main.js
+++ src/main.js
@@ -7,14 +7,34 @@ import dateFilter from '@/filters/date.filter';
 import messagePlugin from '@/utils/message.plugin'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+
 Vue.config.productionTip = false
 
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+    apiKey: 'AIzaSyCoytDEKNHFrMvJOCKcPJsIKKPX44O-RBw',
+    authDomain: 'vue-crm-financial-system.firebaseapp.com',
+    projectId: 'vue-crm-financial-system',
+    storageBucket: 'vue-crm-financial-system.appspot.com',
+    messagingSenderId: '630756236531',
+    appId: '1:630756236531:web:7508b2960a182116a3ab86'
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+    if (!app) {
+        app = new Vue({
+            router,
+            store,
+            render: h => h(App)
+        }).$mount('#app')
+    }
+})
